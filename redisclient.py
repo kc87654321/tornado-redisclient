31bed9ada4375d5cde6b53ef98f3fdda76838d9c
Phus Lu <phus.lu@gmail.com>
2011-10-03 17:37:59 +0800

    bugfix

--- redisclient.py
+++ redisclient.py
@@ -1,14 +1,16 @@
 """non-blocking Redis client interfaces."""
 
 import time
-import collections
 import socket
+import logging
+import collections
+import cStringIO
 
 from tornado.ioloop import IOLoop
 from tornado.iostream import IOStream
 from tornado.util import bytes_type
 
-class RedisPError(Exception):
+class RedisError(Exception):
     def __init__(self, message=None):
         message = message or 'Unknown Redis Error'
         Exception.__init__(self, 'Redis %s' % message)
@@ -20,47 +22,44 @@ def encode(request):
     return data
 
 def decode(data):
-    '''print decode('*4\r\n$3\r\nfoo\r\n$3\r\nbar\r\n$5\r\nhello\r\n$5\r\nworld\r\n')'''
     '''print decode('*4\r\n$3\r\nfoo\r\n$3\r\nbar\r\n$5\r\nhello\r\n:42\r\n')'''
     assert type(data) is bytes_type
-    c = data[0]
+    iodata = cStringIO.StringIO(data)
+    c = iodata.read(1)
     if c == '+':
         return True
     elif c == '-':
-        raise RedisPError(data[1:].rstrip())
+        return iodata.readline().rstrip()
     elif c == ':':
-        return int(data[1:])
+        return int(iodata.readline())
     elif c == '$':
-        if data[:3] == '$-1':
+        number = int(iodata.readline())
+        if number == -1:
             return None
         else:
-            pos = data.find('\r\n')
-            number = int(data[1:pos])
-            return data[pos+2:pos+2+number]
+            data = iodata.read(number)
+            iodata.read(2)
+            return data
     elif c == '*':
-        if data[:3] == '*-1':
+        number = int(iodata.readline())
+        if number == -1:
             return None
         else:
             result = []
-            pos = data.find('\r\n')
-            number = int(data[1:pos])
-            pos1 = pos2 = pos + 2
             while number:
-                if data[pos1] == '$':
-                    pos2    = data.find('\r\n', pos1)
-                    length  = int(data[pos1+1:pos2])
-                    element = data[pos2+2:pos2+2+length]
-                    pos1    = pos2 + length + 4
+                c = iodata.read(1)
+                if c == '$':
+                    length  = int(iodata.readline())
+                    element = iodata.read(length)
+                    iodata.read(2)
                     result.append(element)
                 else:
-                    pos2    = data.find('\r\n', pos1)
-                    element = data[pos1+1:pos2]
-                    pos1    = pos2 + 2
+                    element = iodata.readline()[:-2]
                     result.append(element)
                 number -= 1
             return result
     else:
-        raise RedisPError('Unknown Redis bulk startswith %r', c)
+        raise RedisError('Unknown Redis bulk startswith %r', c)
 
 class AsyncRedisClient(object):
     '''http://ordinary.iteye.com/blog/1097456'''
@@ -108,7 +107,6 @@ class AsyncRedisClient(object):
             if self._subscribe_callback and not self.stream.reading():
                 self._on_write()
 
-
     def _on_write(self):
         self.stream.read_until(bytes_type('\r\n'), self._on_read_first_line)
 
@@ -155,7 +153,6 @@ def main():
     def handle_result(result):
         print 'Redis reply: %r' % result
     redis_client = AsyncRedisClient(('127.0.0.1', 6379))
-
     redis_client.fetch(('LPUSH', 'l', 1), handle_result)
     redis_client.fetch(('LPUSH', 'l', 2), handle_result)
     redis_client.fetch(('LRANGE', 'l', 0, -1), handle_result)
